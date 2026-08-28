8ae34d196e381a8ca3aa8c23cd295b64f2e9ad54
sylrdev <sylrdev@gmail.com>
2026-08-28 11:27:23 +0200

    feat: add mini pick neovim plugin

--- modules/programming/home.nix
+++ modules/programming/home.nix
@@ -6,11 +6,17 @@
     defaultEditor = true;
     plugins = with pkgs.vimPlugins; [
       nvim-tree-lua
+      mini-pick
 
       # dependencies
-      nvim-web-devicons
+      nvim-web-devicons # nvim-tree-lua
     ];
   };
+
+  home.packages = with pkgs; [
+    # dependencies
+    ripgrep # vimPlugins.mini-pick
+  ];
 
   xdg.configFile."nvim".source = ./nvim-config;
 }
